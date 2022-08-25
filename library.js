269a5fad49b7ea4bfe2d347104f226830f398e39
lorenzohill <107278286+lorenzohill@users.noreply.github.com>
2022-08-25 14:49:02 -0500

    Created Modal Menu and Book Card Template

--- library.js
+++ library.js
@@ -2,6 +2,17 @@ console.log("JS library is Setup")
 
 let myLibrary = []
 
+const modalButton = document.querySelector(".modalButton")
+modalButton.addEventListener("click", showModal)
+
+const addButton = document.querySelector(".addButton")
+addButton.addEventListener("click", function(){addBookToLibrary()})
+
+
+//Selectors
+
+const bookHolder = document.querySelector(".bookcontainer")
+
 // Book constructer
 
 function book(title, author, pageCount, isRead) {
@@ -12,15 +23,22 @@ function book(title, author, pageCount, isRead) {
 }
 
 function addBookToLibrary() {
-  let title = prompt("Name a book: ")
-  let author = prompt("Author?")
-  let pageCount = prompt("How Many Pages?")
-  let isRead = prompt("Have you read it?")
+// let title = prompt("Name a book: ")
+// let author = prompt("Author?")
+// let pageCount = prompt("How Many Pages?")
+// let isRead = prompt("Have you read it?")
 
   let book1 = new book(title,author,pageCount,isRead)
   let output = ("The book is " + book1.title + " by " + book1.author+ ". it is " +  book1.pageCount + " pages long.")
   document.querySelector(".word").innerHTML = (output)
 
 }
+
+function showModal() {
+  let show = document.querySelector(".modal")
+  show.classList.toggle("hide")
+
+}
+
 
 addBookToLibrary()
